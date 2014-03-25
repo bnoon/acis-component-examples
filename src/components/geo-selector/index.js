0f0350ffe706b69453d7b39465a7cc553db14b12
Bill Noon <wn10@cornell.edu>
2014-03-25 14:34:22 -0400

    move geo-selector to a component

---
+++ src/components/geo-selector/index.js
@@ -0,0 +1,44 @@
+var request = require('superagent')
+
+module.exports = {
+    template: require('./template.html'),
+
+    data: {
+      state: 'NY',
+      gtypes: [
+        {name: "County", param: "county"},
+        {name: "River Basin", param: "basin"},
+        {name: "Climate Division", param: "climdiv"}
+      ],
+      gtype: '',
+      gids: [],
+      gid: ''
+    },
+
+    ready: function () {
+      this.$watch('gtype', function(x) {
+        this.gid = ''
+        this.$emit('clear')
+        this.geoload()
+      });
+      this.$watch('gid', function() {
+        if (this.gid) this.$emit('params',{gtype:this.gtype,gid:this.gid})
+      });
+    },
+
+    methods: {
+      geoload: function () {
+        var self=this, params = {state: this.state, meta: ['name','id']}
+        request
+          .post('http://data.rcc-acis.org/General/'+this.gtype, params)
+          .end(function(res){
+            if (res.status == 200) {
+              if (res.body.error === undefined) self.gids = res.body.meta
+              else console.log('invalid request %s', res.body.error)
+            } else {
+              console.log('failed reqest %s',res.status)
+            }
+          })
+      }
+    }
+}
